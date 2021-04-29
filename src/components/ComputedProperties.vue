92f2700ddb5456e67eb3ceba11e257c9b1a2b126
javierpb275 <javierpb275@gmail.com>
2021-04-29 20:01:17 +0200

    In-template expressions are very convenient, but they are meant for simple operations. Putting too much logic in your templates can make them bloated and hard to maintain. For example:
    <div id="example">
      {{ message.split('').reverse().join('') }}
    </div>
    At this point, the template is no longer simple and declarative. You have to look at it for a second before realizing that it displays message in reverse. The problem is made worse when you want to include the reversed message in your template more than once.
    
    That’s why for any complex logic, you should use a computed property.
    
    Basic Example
    <div id="example">
      <p>Original message: "{{ message }}"</p>
      <p>Computed reversed message: "{{ reversedMessage }}"</p>
    </div>
    var vm = new Vue({
      el: '#example',
      data: {
        message: 'Hello'
      },
      computed: {
        // a computed getter
        reversedMessage: function () {
          // `this` points to the vm instance
          return this.message.split('').reverse().join('')
        }
      }
    })
    Result:
    
    Here we have declared a computed property reversedMessage. The function we provided will be used as the getter function for the property vm.reversedMessage:
    
    console.log(vm.reversedMessage) // => 'olleH'
    vm.message = 'Goodbye'
    console.log(vm.reversedMessage) // => 'eybdooG'
    You can open the console and play with the example vm yourself. The value of vm.reversedMessage is always dependent on the value of vm.message.
    
    You can data-bind to computed properties in templates just like a normal property. Vue is aware that vm.reversedMessage depends on vm.message, so it will update any bindings that depend on vm.reversedMessage when vm.message changes. And the best part is that we’ve created this dependency relationship declaratively: the computed getter function has no side effects, which makes it easier to test and understand.

--- src/components/ComputedProperties.vue
+++ src/components/ComputedProperties.vue
@@ -1,15 +1,25 @@
 <template>
  <div class="computed-properties">
-     COMPUTED PROPERTIES
+    <p>Original message: {{ message }}</p>
+    <p>Computed reversed message: {{ reversedMessage }}</p>
  </div>
 </template>
  
 <script>
 export default {
   name:'ComputedProperties',
+  data() {
+    return {
+      message: 'Hello'
+    }
+  },
   //when you need to change the presentation of existing data, you'll use computed properties
   computed: {
-
+    // a computed getter
+    reversedMessage() {
+      // `this` points to the vm instance
+      return this.message.split('').reverse().join('')
+    }
   },
   //when you need to change data, you will use methods
   methods: {
