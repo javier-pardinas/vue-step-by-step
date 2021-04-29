d91673d27f181d797e43776ec8e33ca12959b0eb
javierpb275 <javierpb275@gmail.com>
2021-04-29 18:22:15 +0200

    The double mustaches interprets the data as plain text, not HTML. In order to output real HTML, you will need to use the v-html directive
    The contents of the span will be replaced with the value of the rawHtml property, interpreted as plain HTML - data bindings are ignored. Note that you cannot use v-html to compose template partials, because Vue is not a string-based templating engine. Instead, components are preferred as the fundamental unit for UI reuse and composition.
    
    Dynamically rendering arbitrary HTML on your website can be very dangerous because it can easily lead to XSS vulnerabilities. Only use HTML interpolation on trusted content and never on user-provided content.

--- src/components/AppOne.vue
+++ src/components/AppOne.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="app-one">
   MESSAGE: {{message}}<!--accesing data and display it-->
+  <p>Using mustaches: {{ rawHtml }}</p><!--The double mustaches interprets the data as plain text, not HTML-->
+  <p>Using v-html directive: <span v-html="rawHtml"></span></p><!--In order to output real HTML, you will need to use the v-html directive-->
   </div>
 </template>
 
@@ -10,7 +12,8 @@ name: 'AppOne',
   //data: object that defines the component. we can access this properties in our template
   data() {
     return {
-      message: 'Hello Vue!'
+      message: 'Hello Vue!',
+      rawHtml: '<span style="color:red">This should be red</span>'
     }
   },
 }
