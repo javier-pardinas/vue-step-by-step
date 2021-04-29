e8330611819271ef012ce16e545b79b1ceedeaae
javier-pardinas <javierpb275@gmail.com>
2021-04-29 11:58:03 +0200

    the v-for directive can be used for displaying a list of items using the data from an Array

--- src/components/AppFour.vue
+++ src/components/AppFour.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="app-four">
-   App4
+     <ol>
+    <li v-for="(todo, i) in todos" v-bind:key="i"><!--v-for: so we can loop through the array of objects and access the properties of each object-->
+      {{ todo.text }}
+    </li>
+  </ol>
   </div>
 </template>
 
@@ -10,7 +14,11 @@ name: 'AppFour',
   //data: object that defines the component. we can access this properties in our template
   data() {
     return {
-      
+      todos: [
+      { text: 'Learn JavaScript' },
+      { text: 'Learn Vue' },
+      { text: 'Build something awesome' }
+    ]
     }
   },
 }
