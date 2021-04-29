878230d77e3070f61586ec5abf82f7d56f226968
javier-pardinas <javierpb275@gmail.com>
2021-04-29 12:13:34 +0200

    Note that in this method we update the state of our app without touching the DOM - all DOM manipulations are handled by Vue, and the code you write is focused on the underlying logic.

--- src/components/AppFive.vue
+++ src/components/AppFive.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="app-five">
-    App5
+    <p>{{ message }}</p>
+    <button v-on:click="reverseMessage">Reverse Message</button><!--To let users interact with your app, we can use the v-on directive to attach event listeners that invoke methods on our Vue instances-->
   </div>
 </template>
 
@@ -9,7 +10,16 @@ export default {
 name: 'AppFive',
   //data: object that defines the component. we can access this properties in our template
   data() {
-      
+    return {
+      message: 'Hello Vue.js!'
+    }
+  },
+  //Here we put the methods that we are gonna use in this component
+  methods: {
+    //This method reverse the message objetc of data()
+    reverseMessage() {
+      this.message = this.message.split('').reverse().join('')
+    }
   }
 }
 </script>
