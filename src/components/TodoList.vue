a8327e3e3defadf544d8719ab73baf247c94611f
javierpb275 <javierpb275@gmail.com>
2021-04-29 19:09:33 +0200

    Starting in version 2.6.0, it is also possible to use a JavaScript expression in a directive argument by wrapping it with square brackets:
    <!--
    Note that there are some constraints to the argument expression, as explained
    in the "Dynamic Argument Expression Constraints" section below.
    -->
    <a v-bind:[attributeName]="url"> ... </a>
    Here attributeName will be dynamically evaluated as a JavaScript expression, and its evaluated value will be used as the final value for the argument. For example, if your Vue instance has a data property, attributeName, whose value is "href", then this binding will be equivalent to v-bind:href.
    
    Similarly, you can use dynamic arguments to bind a handler to a dynamic event name:
    
    <a v-on:[eventName]="doSomething"> ... </a>
    In this example, when eventName‘s value is "focus", v-on:[eventName] will be equivalent to v-on:focus.

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -8,6 +8,11 @@
       v-bind:key="item.id"/>
      </ol>
      <a v-bind:href="url"> Vue.js Guide </a><!--Here href is the argument, which tells the v-bind directive to bind the element’s href attribute to the value of the expression url-->
+      <br>
+      <!--Dynamic Arguments: v-bind:[myHref], v-on:[myEvent]-->
+     <a v-bind:[myHref]="url"> Vue.js Guide </a>
+     <br>
+     <a v-on:[myEvent]="alertHello"> Alert Hello </a>
  </div>
 </template>
  
@@ -23,11 +28,19 @@ export default {
       { id: 1, text: 'Cheese' },
       { id: 2, text: 'Whatever else humans are supposed to eat' }
      ],
-     url: "https://vuejs.org/v2/guide/syntax.html"
+     url: "https://vuejs.org/v2/guide/syntax.html",
+    //Dynamic Arguments: v-bind:[myHref], v-on:[myEvent]
+     myHref: "href",
+     myEvent: "click"
     }
   },
   components: {
     TodoItem
+  },
+  methods: {
+    alertHello() {
+      alert("Hello");
+    }
   }
 }
 </script>
