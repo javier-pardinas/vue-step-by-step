d8668d513b0527170865110565c0e87eaf62ef30
javier-pardinas <javierpb275@gmail.com>
2021-04-29 10:39:30 +0200

    added mounted() method that gets executed everytime the components rerenders (the state (data) changes)

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,16 +1,25 @@
 <template>
   <div class="counter">
-  COUNTER: {{counter}}
+  COUNTER: {{counter}}<!--accesing data and display it-->
   </div>
 </template>
 
 <script>
 export default {
   name: 'Counter',
+  //data: object that defines the component. we can access this properties in our template
   data() {
     return {
       counter: 0
     }
+  },
+  //mounted: like componentDidMount(), this gets executed everytime the component is rerendered
+  //everytime there is a change in the state of the component (data())
+  mounted() {
+    //this method sums 1 to the counter every second
+    setInterval(() => {
+      this.counter++
+    }, 1000)
   }
 }
 </script>
