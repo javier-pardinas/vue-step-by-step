31bd91b6f03fe8c3b32788fdb558d98d432e0cb2
javier-pardinas <javierpb275@gmail.com>
2021-04-29 11:35:46 +0200

    Here we are encountering something new. The v-bind attribute you are seeing is called a directive. Directives are prefixed with v- to indicate that they are special attributes provided by Vue, and as you may have guessed, they apply special reactive behavior to the rendered DOM. Here, it is basically saying “keep this element’s title attribute up-to-date with the message property on the Vue instance.”

--- src/components/AppTwo.vue
+++ src/components/AppTwo.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="app-two">
-  App 2
+  <span v-bind:title="message"><!--with v-bind:title="message" when we hover over whats in the span we see the message of data()-->
+    Hover your mouse over me for a few seconds
+    to see my dynamically bound title!
+  </span>
   </div>
 </template>
 
@@ -10,7 +13,7 @@ name: 'AppTwo',
   //data: object that defines the component. we can access this properties in our template
   data() {
     return {
-      message: 'Hello Vue!'
+      message: 'You loaded this page on ' + new Date().toLocaleString()//new Date().toLocaleString(): 29/04/2021 11:28:41
     }
   },
 }
