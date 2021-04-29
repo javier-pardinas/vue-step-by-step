f1f86d6c05fe6d1931b5bad71050134428258afb
javierpb275 <javierpb275@gmail.com>
2021-04-29 18:39:12 +0200

    So far we’ve only been binding to simple property keys in our templates. But Vue.js actually supports the full power of JavaScript expressions inside all data bindings
    These expressions will be evaluated as JavaScript in the data scope of the owner Vue instance. One restriction is that each binding can only contain one single expression
    
    Template expressions are sandboxed and only have access to a whitelist of globals such as Math and Date. You should not attempt to access user-defined globals in template expressions.

--- src/components/AppTwo.vue
+++ src/components/AppTwo.vue
@@ -4,6 +4,14 @@
     Hover your mouse over me for a few seconds
     to see my dynamically bound title!
   </span>
+  <br>
+  <!--These expressions will be evaluated as JavaScript in the data scope of the owner Vue instance-->
+  <span>{{ number + 1 }}</span>
+  <br>
+  <span>{{ ok ? 'YES' : 'NO' }}</span>
+  <br>
+  <span>{{ message.split('').reverse().join('') }}</span>
+  <div v-bind:id="'myId-' + number"></div><!--Vue.js actually supports the full power of JavaScript expressions inside all data bindings-->
   </div>
 </template>
 
@@ -13,7 +21,9 @@ name: 'AppTwo',
   //data: object that defines the component. we can access this properties in our template
   data() {
     return {
-      message: 'You loaded this page on ' + new Date().toLocaleString()//new Date().toLocaleString(): 29/04/2021 11:28:41
+      message: 'You loaded this page on ' + new Date().toLocaleString(),//new Date().toLocaleString(): 29/04/2021 11:28:41
+      number: 1,
+      ok: true
     }
   },
 }
