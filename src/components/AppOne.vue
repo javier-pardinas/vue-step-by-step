b580b164f8651d21ec30230f1f005d9f8818df6f
javier-pardinas <javierpb275@gmail.com>
2021-04-29 11:22:18 +0200

    added name object (name of component) and data() method (state of the component) which is an object that defines our component. we can use the properties of data() and display them in our template.

--- src/components/AppOne.vue
+++ src/components/AppOne.vue
@@ -1,11 +1,17 @@
 <template>
   <div class="app-one">
-  Hello Vue
+  MESSAGE: {{message}}
   </div>
 </template>
 
 <script>
 export default {
-
+name: 'AppOne',
+  //data: object that defines the component. we can access this properties in our template
+  data() {
+    return {
+      message: 'Hello Vue!'
+    }
+  },
 }
 </script>
