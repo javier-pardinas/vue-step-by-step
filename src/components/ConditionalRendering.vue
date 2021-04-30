cc48889863a0824a79f2edcc929d91c8a8f761b3
javier-pardinas <javierpb275@gmail.com>
2021-04-30 11:06:59 +0200

    Vue offers a way for you to say, “These two elements are completely separate - don’t re-use them.” Add a key attribute with unique values:
    Now those inputs will be rendered from scratch each time you toggle
    
    Note that the <label> elements are still efficiently re-used, because they don’t have key attributes.

--- src/components/ConditionalRendering.vue
+++ src/components/ConditionalRendering.vue
@@ -13,11 +13,11 @@
  <!------------------------------------------>
 <div v-if="loginType === 'username'">
   <label>Username</label>
-  <input placeholder="Enter your username">
+  <input placeholder="Enter your username" key="username-input">
 </div>
 <div v-else>
   <label>Email</label>
-  <input placeholder="Enter your email address">
+  <input placeholder="Enter your email address" key="email-input">
 </div>
 </div>
 </template>
