e161ab72d5a8ed4e499ec3d8f49888c754b199b3
javier-pardinas <javierpb275@gmail.com>
2021-04-30 11:05:22 +0200

    Controlling Reusable Elements with key Vue tries to render elements as efficiently as possible, often re-using them instead of rendering from scratch. Beyond helping make Vue very fast, this can have some useful advantages. For example, if you allow users to toggle between multiple login types:
    <template v-if="loginType === 'username'">
      <label>Username</label>
      <input placeholder="Enter your username">
    </template>
    <template v-else>
      <label>Email</label>
      <input placeholder="Enter your email address">
    </template>
    
    Then switching the loginType in the code above will not erase what the user has already entered. Since both templates use the same elements, the <input> is not replaced - just its placeholder.

--- src/components/ConditionalRendering.vue
+++ src/components/ConditionalRendering.vue
@@ -10,11 +10,25 @@
 <div v-else>
   WELL DONE
 </div>
- </div>
+ <!------------------------------------------>
+<div v-if="loginType === 'username'">
+  <label>Username</label>
+  <input placeholder="Enter your username">
+</div>
+<div v-else>
+  <label>Email</label>
+  <input placeholder="Enter your email address">
+</div>
+</div>
 </template>
  
 <script>
 export default {
-  name:'ConditionalRendering'
+  name:'ConditionalRendering',
+  data() {
+      return {
+          loginType: 'username'
+      }
+  }
 }
 </script>
