6fa9102fc8053bb76818ec2ae0a181f975fcdf55
javier-pardinas <javierpb275@gmail.com>
2021-04-30 09:51:49 +0200

    We can pass an object to v-bind:class to dynamically toggle classes:
    <div v-bind:class="{ active: isActive }"></div>
    The above syntax means the presence of the active class will be determined by the truthiness of the data property isActive.
    
    You can have multiple classes toggled by having more fields in the object. In addition, the v-bind:class directive can also co-exist with the plain class attribute. So given the following template:
    
    <div
      class="static"
      v-bind:class="{ active: isActive, 'text-danger': hasError }"
    ></div>
    And the following data:
    
    data: {
      isActive: true,
      hasError: false
    }
    It will render:
    
    <div class="static active"></div>
    When isActive or hasError changes, the class list will be updated accordingly. For example, if hasError becomes true, the class list will become "static active text-danger".

--- src/components/ClassStyleBindings.vue
+++ src/components/ClassStyleBindings.vue
@@ -1,11 +1,34 @@
 <template>
   <div class="class-style-bindings">
-  ClassStyleBindings
+  <!--We can pass an object to v-bind:class to dynamically toggle classes:-->
+    <div class="static"
+    v-bind:class="{ 'active': isActive, 'text-danger': hasError }">
+    ClassStyleBindings
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-name: 'ClassStyleBindings'
+name: 'ClassStyleBindings',
+data() {
+    return {
+        //booleans for style classes:
+        isActive: false,
+        hasError: true
+    }
+}
 }
 </script>
+
+<style>
+.static {
+text-transform: uppercase;
+}
+.active {
+color: green;
+}
+.text-danger {
+color:red;
+}
+</style>
