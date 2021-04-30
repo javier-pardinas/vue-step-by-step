3c33925f3286c557af5e97ef6f592dda2b1d183e
javier-pardinas <javierpb275@gmail.com>
2021-04-30 15:05:44 +0200

    Method Event Handlers
    The logic for many event handlers will be more complex though, so keeping your JavaScript in the value of the v-on attribute isn’t feasible. That’s why v-on can also accept the name of a method you’d like to call.

--- src/components/EventHandling.vue
+++ src/components/EventHandling.vue
@@ -3,6 +3,8 @@
     <div id="example-1">
         <button v-on:click="counter += 1">Add 1</button>
         <p>The button above has been clicked {{ counter }} times.</p>
+        <!-- `greet` is the name of a method defined below -->
+        <button v-on:click="greet">Greet</button>
     </div>
   </div>
 </template>
@@ -12,8 +14,19 @@ export default {
 name: 'EventHandling',
 data() {
     return {
-        counter: 0
+        counter: 0,
+        name: 'Vue.js'
     }
+},
+methods: {
+  greet(event) {
+    // `this` inside methods points to the Vue instance
+      alert('Hello ' + this.name + '!')
+      // `event` is the native DOM event
+      if (event) {
+        alert(event.target.tagName)
+      }
+  }
 }
 }
 </script>
