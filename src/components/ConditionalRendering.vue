21dbe3c3a61b8339d4b63d6c3fc4b2dc914c370a
javier-pardinas <javierpb275@gmail.com>
2021-04-30 10:57:12 +0200

    conditionals v-if, v-else-if and v-else

--- src/components/ConditionalRendering.vue
+++ src/components/ConditionalRendering.vue
@@ -1,7 +1,15 @@
 <!--Computed properties are by default getter-only, but you can also provide a setter when you need it:-->
 <template>
  <div class="conditional-rendering">
-    Conditional rendering
+<div v-if="Math.random() <= 0.5">
+  FAIL
+</div>
+<div v-else-if="Math.random() <= 0.9">
+  PASS
+</div>
+<div v-else>
+  WELL DONE
+</div>
  </div>
 </template>
  
