886925c0362a8910b3d50a5bfbf1063c46c68b63
javier-pardinas <javierpb275@gmail.com>
2021-04-30 13:54:38 +0200

    If instead, your intent is to conditionally skip execution of the loop, you can place the v-if on a wrapper element (or <template>). For example:
    <ul v-if="todos.length">
      <li v-for="todo in todos">
        {{ todo }}
      </li>
    </ul>
    <p v-else>No todos left!</p>

--- src/components/ForWithIf.vue
+++ src/components/ForWithIf.vue
@@ -3,11 +3,35 @@
   <!--  <li v-for="todo in todos" :key="todo" v-if="!todo.isComplete" >
   {{ todo }}
     </li>-->
+    <ul v-if="todos.length">
+        <li v-for="todo in todos" :key="todo.id">
+        {{ todo.task }}
+        </li>
+    </ul>
+    <p v-else>No todos left!</p>
   </div>
 </template>
 
 <script>
 export default {
-name: 'ForWithIf'
+name: 'ForWithIf',
+data() {
+    return {
+        todos: [
+            {
+                id: 1,
+                task: "Do Something"
+            },
+            {
+                id: 2,
+                task: "Do another thing"
+            },
+            {
+                id: 3,
+                task: "And the other thing..."
+            }
+        ]
+    }
+}
 }
 </script>
