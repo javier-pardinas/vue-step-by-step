edb48d4505846a72091ac212660fa48bd0639ba7
javierpb275 <javierpb275@gmail.com>
2021-05-01 10:06:29 +0200

    Methods in Inline Handlers Instead of binding directly to a method name, we can also use methods in an inline JavaScript statement:
    <div id="example-3">
      <button v-on:click="say('hi')">Say hi</button>
      <button v-on:click="say('what')">Say what</button>
    </div>
    new Vue({
      el: '#example-3',
      methods: {
        say: function (message) {
          alert(message)
        }
      }
    })
    
    Sometimes we also need to access the original DOM event in an inline statement handler. You can pass it into a method using the special $event variable:
    
    <button v-on:click="warn('Form cannot be submitted yet.', $event)">
      Submit
    </button>
    // ...
    methods: {
      warn: function (message, event) {
        // now we have access to the native event
        if (event) {
          event.preventDefault()
        }
        alert(message)
      }
    }

--- src/components/EventHandling.vue
+++ src/components/EventHandling.vue
@@ -5,6 +5,12 @@
         <p>The button above has been clicked {{ counter }} times.</p>
         <!-- `greet` is the name of a method defined below -->
         <button v-on:click="greet">Greet</button>
+        <!--Instead of binding directly to a method name, we can also use methods in an inline JavaScript statement. 
+        Sometimes we also need to access the original DOM event in an inline statement handler. 
+        You can pass it into a method using the special $event variable-->
+        <button v-on:click="warn('Form cannot be submitted yet.', $event)">
+         Submit
+        </button>
     </div>
   </div>
 </template>
@@ -26,6 +32,13 @@ methods: {
       if (event) {
         alert(event.target.tagName)
       }
+  },
+  warn(message, event) {
+    // now we have access to the native event
+    if (event) {
+      event.preventDefault()
+    }
+    alert(message)
   }
 }
 }
