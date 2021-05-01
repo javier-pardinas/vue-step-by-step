f3e548ddebf23505ce0416d297fa8b26cefff532
javierpb275 <javierpb275@gmail.com>
2021-05-01 10:15:09 +0200

    Event Modifiers It is a very common need to call event.preventDefault() or event.stopPropagation() inside event handlers. Although we can do this easily inside methods, it would be better if the methods can be purely about data logic rather than having to deal with DOM event details.
    To address this problem, Vue provides event modifiers for v-on. Recall that modifiers are directive postfixes denoted by a dot.
    
    .stop
    .prevent
    .capture
    .self
    .once
    .passive
    
    Order matters when using modifiers because the relevant code is generated in the same order. Therefore using v-on:click.prevent.self will prevent all clicks while v-on:click.self.prevent will only prevent clicks on the element itself.

--- src/components/EventHandling.vue
+++ src/components/EventHandling.vue
@@ -11,6 +11,24 @@
         <button v-on:click="warn('Form cannot be submitted yet.', $event)">
          Submit
         </button>
+        <!--It is a very common need to call event.preventDefault() or event.stopPropagation() inside event handlers. 
+        Although we can do this easily inside methods, it would be better if the methods can be purely about data logic 
+        rather than having to deal with DOM event details. To address this problem, Vue provides event modifiers for v-on. 
+        Recall that modifiers are directive postfixes denoted by a dot.-->
+        <!-- the click event's propagation will be stopped -->
+        <a v-on:click.stop="doThis"></a>
+        <!-- the submit event will no longer reload the page -->
+        <form v-on:submit.prevent="onSubmit"></form>
+        <!-- modifiers can be chained -->
+        <a v-on:click.stop.prevent="doThat"></a>
+        <!-- just the modifier -->
+        <form v-on:submit.prevent></form>
+        <!-- use capture mode when adding the event listener -->
+        <!-- i.e. an event targeting an inner element is handled here before being handled by that element -->
+        <div v-on:click.capture="doThis">...</div>
+        <!-- only trigger handler if event.target is the element itself -->
+        <!-- i.e. not from a child element -->
+        <div v-on:click.self="doThat">...</div>
     </div>
   </div>
 </template>
