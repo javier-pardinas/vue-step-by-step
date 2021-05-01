8201df2cde70b436daa8a7cd93fd38ebc1f9877e
javierpb275 <javierpb275@gmail.com>
2021-05-01 10:19:40 +0200

    Unlike the other modifiers, which are exclusive to native DOM events, the .once modifier can also be used on component events.
    Vue also offers the .passive modifier, corresponding to addEventListener‘s passive option.
    
    The .passive modifier is especially useful for improving performance on mobile devices.
    
    Don’t use .passive and .prevent together, because .prevent will be ignored and your browser will probably show you a warning. Remember, .passive communicates to the browser that you don’t want to prevent the event’s default behavior.

--- src/components/EventHandling.vue
+++ src/components/EventHandling.vue
@@ -29,6 +29,12 @@
         <!-- only trigger handler if event.target is the element itself -->
         <!-- i.e. not from a child element -->
         <div v-on:click.self="doThat">...</div>
+        <!-- the click event will be triggered at most once -->
+        <a v-on:click.once="doThis"></a>
+        <!-- the scroll event's default behavior (scrolling) will happen -->
+        <!-- immediately, instead of waiting for `onScroll` to complete  -->
+        <!-- in case it contains `event.preventDefault()`-->
+        <div v-on:scroll.passive="onScroll">...</div>
     </div>
   </div>
 </template>
