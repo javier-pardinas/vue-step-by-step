8541f420d3f9cd6181b2c1827e6a1cdd0343ec00
javierpb275 <javierpb275@gmail.com>
2021-05-01 18:36:11 +0200

    created navslot and added router-link for each view. this router-link tags take us to the path that we add in the to="/".

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,30 @@
 <template>
   <div id="app">
+    <NavSlot>
+      <li class="nav-item">
+        <router-link class="nav-link" to="/">Home</router-link>
+      </li>
+      <li class="nav-item">
+        <router-link class="nav-link" to="/blog">Blog</router-link>
+      </li>
+      <li class="nav-item">
+        <router-link class="nav-link" to="/services">Services</router-link>
+      </li>
+      <li class="nav-item">
+      <router-link class="nav-link" to="/contact">Contact</router-link>
+      </li>
+    </NavSlot>
     <router-view/>
   </div>
 </template>
 
 <script>
+import NavSlot from '@/components/NavSlot';
 
 export default {
   name: 'App',
   components: {
+    NavSlot
   },
 }
 </script>
---
+++ src/components/NavSlot.vue
@@ -0,0 +1,19 @@
+<template>
+ <div class="nav-slot">
+     <nav>
+         <ul>
+             <slot></slot>
+         </ul>
+     </nav>
+ </div>
+</template>
+
+<script>
+ export default{
+  name:'NavSlot',
+ }
+</script>
+
+<style scoped>
+ 
+</style>
