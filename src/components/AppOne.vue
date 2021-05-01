de6cd92ef9993935817f1c3c2ea372a7766b4526
javierpb275 <javierpb275@gmail.com>
2021-05-01 16:18:27 +0200

    Slot Example

--- src/components/AppOne.vue
+++ src/components/AppOne.vue
@@ -3,12 +3,20 @@
   MESSAGE: {{message}}<!--accesing data and display it-->
   <p>Using mustaches: {{ rawHtml }}</p><!--The double mustaches interprets the data as plain text, not HTML-->
   <p>Using v-html directive: <span v-html="rawHtml"></span></p><!--In order to output real HTML, you will need to use the v-html directive-->
+  <SlotExample>
+    Vue.js Introduction Slots
+  </SlotExample>
   </div>
 </template>
 
 <script>
+import SlotExample from '@/components/SlotExample';
+
 export default {
 name: 'AppOne',
+  components: {
+    SlotExample
+  },
   //data: object that defines the component. we can access this properties in our template
   data() {
     return {
---
+++ src/components/SlotExample.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="slot-example">
+    <a href="https://vuejs.org/v2/guide/components-slots.html" class="nav-link">
+    <slot></slot>
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+name: 'SlotExample'
+}
+</script>
