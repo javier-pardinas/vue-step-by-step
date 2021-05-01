67f77ee9ee33d158e85ea2bf0ade124b8177ab0a
javierpb275 <javierpb275@gmail.com>
2021-05-01 12:32:14 +0200

    For radio, checkbox and select options, the v-model binding values are usually static strings (or booleans for checkboxes)
    But sometimes, we may want to bind the value to a dynamic property on the Vue instance. We can use v-bind to achieve that. In addition, using v-bind allows us to bind the input value to non-string values.

--- src/components/FormInputBindings.vue
+++ src/components/FormInputBindings.vue
@@ -1,16 +1,20 @@
 <template>
   <div class="form-input-bindings">
+
     <!--Text-->
     <input v-model="message1" placeholder="edit me">
     <p>Message is: {{ message1 }}</p>
+
     <!--Multiline Text-->
     <span>Multiline message is:</span>
     <p style="white-space: pre-line;">{{ message2 }}</p>
     <br>
     <textarea v-model="message2" placeholder="add multiple lines"></textarea>
+
     <!--Single checkbox-->
     <input type="checkbox" id="checkbox" v-model="checked">
     <label for="checkbox">{{ checked }}</label>
+
     <!--Multiple Checkbox-->
     <input type="checkbox" id="jack" value="Jack" v-model="checkedNames">
     <label for="jack">Jack</label>
@@ -20,6 +24,7 @@
     <label for="mike">Mike</label>
     <br>
     <span>Checked names: {{ checkedNames }}</span>
+
     <!--Radio-->
     <input type="radio" id="one" value="One" v-model="picked">
     <label for="one">One</label>
@@ -28,6 +33,7 @@
     <label for="two">Two</label>
     <br>
     <span>Picked: {{ picked }}</span>
+
     <!--Single Select-->
     <select v-model="selected1">
         <option disabled value="">Please select one</option>
@@ -36,6 +42,7 @@
         <option>C</option>
     </select>
     <span>Selected: {{ selected1 }}</span>
+
     <!--Multiple select (bound to Array):-->
     <select v-model="selected2" multiple>
         <option>A</option>
@@ -44,6 +51,7 @@
     </select>
     <br>
     <span>Selected: {{ selected2 }}</span>
+
     <!--Dynamic options rendered with v-for-->
     <select v-model="selected3">
         <option v-for="option in options" v-bind:value="option.value" :key="option">
@@ -51,6 +59,18 @@
         </option>
     </select>
     <span>Selected: {{ selected3 }}</span>
+
+    <!-- `picked` is a string "a" when checked -->
+    <input type="radio" v-model="picked" value="a">
+
+    <!-- `toggle` is either true or false -->
+    <input type="checkbox" v-model="toggle">
+
+    <!-- `selected` is a string "abc" when the first option is selected -->
+    <select v-model="selected">
+    <option value="abc">ABC</option>
+    </select>
+    
   </div>
 </template>
 
